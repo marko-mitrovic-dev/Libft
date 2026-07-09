86780caa3d5a0a6408d447200234e9fc46946c15
Mitre341 <emarkomitrovic@gmail.com>
2026-07-09 15:39:31 +0200

    added ft_strrchr.c to Makefile

--- libft.h
+++ libft.h
@@ -20,5 +20,6 @@ size_t ft_strlcat(char *dest, const char *src, size_t destsize);
 int ft_toupper(int c);
 int ft_tolower(int c);
 char *ft_strchr(const char *s, int c);
+char *ft_strrchr(const char *s, int c);
 
 # endif
