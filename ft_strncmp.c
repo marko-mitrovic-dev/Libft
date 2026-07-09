e34ed2b0e7475792d7e4b59472337ba2a270b135
Mitre341 <emarkomitrovic@gmail.com>
2026-07-09 16:47:53 +0200

    Finished ft_strncmp.c added to header and make

---
+++ ft_strncmp.c
@@ -0,0 +1,44 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "libft.h"
+
+
+
+
+int ft_strncmp(const char *s1, const char *s2, size_t n)
+{   
+    size_t i;
+
+    i = 0;
+    while (i < n)
+    {
+        if (s1[i] != s2[i])
+        {
+            return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+        }
+        if(s1[i] == '\0')
+        {
+            return (0);
+        }
+        i++;
+    }
+
+    return(0);
+}
+
+int main(){
+    char s1[] = "Marko";
+    char s2[] = "Marco";
+
+    printf("this is string number compare %s and %s\n after function %d\n", s1, s2, ft_strncmp(s1, s2, 4));
+
+}
+
+/*
+This function just compare 2 strings for n bytes
+string number compare strncmp
+changing character to unsigned char just when deduct becuase 
+just there I am comparing 2 characters before s1 is just a pointer
+
+*/
