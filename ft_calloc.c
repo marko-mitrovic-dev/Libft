119d07c1d08410def175160b05c21bb4e1a492c2
Mitre341 <emarkomitrovic@gmail.com>
2026-07-10 17:15:38 +0200

    Adding ft_calloc.c

---
+++ ft_calloc.c
@@ -0,0 +1,60 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "libft.h"
+
+
+void *ft_memset(void *b, int c, size_t len){
+
+    size_t i;
+    unsigned char *ptr;
+
+    i = 0;
+    ptr = (unsigned char *) b;
+
+    while (i < len){
+
+        ptr[i] = (unsigned char) c;
+        i++;
+    }
+
+    return(b);
+}
+
+void *ft_calloc(size_t nmemb, size_t size)
+{
+    size_t totall_number_of_bytes = nmemb * size;
+    void *ptr = malloc(totall_number_of_bytes);
+
+    if (ptr == NULL)
+        return (NULL);
+
+    ft_memset(ptr, 0, totall_number_of_bytes);
+
+    return(ptr);
+
+}
+int main()
+{
+    // Alociramo niz od 3 karaktera
+    char *str = (char *)ft_calloc(3, sizeof(char));
+    
+    if (str != NULL)
+    {
+        // Štampamo vrednosti na indeksima 0, 1 i 2 da proverimo da li su nule
+        printf("Indeks 0: %d\n", str[0]);
+        printf("Indeks 1: %d\n", str[1]);
+        printf("Indeks 2: %d\n", str[2]);
+        
+        free(str); // Oslobodi memoriju na kraju
+    }
+    return (0);
+}
+/*
+nmemb je skraćeno od "Number of members" (ili Number of elements)
+malloc ti da "prljavu" memoriju (unutra može da bude bilo kakvo đubre
+ koje je ranije ostalo u RAM-u). calloc kompletnu memoriju očisti i postavi na nule (0).
+ Zato se i zove Calloc (Cleared Allocation).
+ char *str = malloc(11 * sizeof(char)); 10 + 1
+ char *str = calloc(11, sizeof(char));
+*/
