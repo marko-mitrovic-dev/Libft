64da7ec9fbde1ed9da3a8096258cdee37cbe7304
Mitre341 <emarkomitrovic@gmail.com>
2026-07-25 16:00:51 +0200

    added ft_strtrim.c

--- libft.h
+++ libft.h
@@ -31,6 +31,7 @@ int ft_atoi(const char *nptr);
 void *ft_calloc(size_t nmemb, size_t size);
 char *ft_substr(char const *s, unsigned int start, size_t len);
 char *ft_strjoin(char const *s1, char const *s2);
+char *ft_strtrim(char const *s1, char const *set);
 
 
 # endif
