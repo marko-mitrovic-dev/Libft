e5c9c2e1db769e43e42f646a67d5f5736187186d
Mitre341 <emarkomitrovic@gmail.com>
2026-07-25 19:16:02 +0200

    ft_itoa.c is done. lets see next function

---
+++ ft_itoa.c
@@ -0,0 +1,78 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "libft.h"
+
+
+
+static int get_len(long n)
+{
+    int len;
+
+    len = 0;
+    if (n <= 0)
+        len++;
+
+    while (n != 0)
+    {
+        n = n / 10;
+        len++;
+    }
+    return (len);
+}
+
+
+
+char *ft_itoa(int n)
+{
+   int len;
+   char *str;
+   long num;
+
+
+   num = n;
+   len = get_len(n);
+   str = malloc ((len + 1) * sizeof(char));
+   if (str == NULL)
+   {
+    return(NULL);
+   }
+
+    str[len] = '\0';
+
+    if(num == 0)
+        str[0] = '0';
+
+    if (num < 0 )
+    {
+        str[0] = '-';
+        num = -num;
+    }
+
+
+    while (num > 0)
+    {
+        len--;
+        str[len] = num % 10 + '0';
+        num = num / 10;
+    }
+
+    return (str);
+
+}
+
+
+int main()
+{
+    int number = -555;
+    char *string;
+
+    string = ft_itoa(number);
+
+    printf("Now printing characters %s \n", string);
+
+    free(string);
+    return(0);
+
+
+}
