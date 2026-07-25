8d9e61f2a7dfed7ac01940fc98626f84ba913a7e
Mitre341 <emarkomitrovic@gmail.com>
2026-07-25 18:17:33 +0200

    ft_split.c done, but need some corrections for tomorrow

---
+++ ft_split.c
@@ -0,0 +1,101 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "libft.h"
+
+
+static int  count_words(char const *s, char c)
+{
+    int count;
+    int in_word;
+
+    count = 0;
+    in_word = 0;
+
+    while(*s)
+    {
+        if (*s != c && in_word == 0)
+        {
+            in_word = 1;
+            count++;
+        }
+        else if (*s == c)
+            in_word = 0;
+        s++;
+    }
+    return(count);
+}
+
+char **ft_split(char const *s, char c)
+{
+
+    char **tab;
+    int i;
+    int len;
+
+    if (s == NULL)
+        return (NULL);
+
+    tab = malloc((count_words(s, c) + 1) *sizeof(char *));
+    if(tab == NULL)
+        return(NULL);
+    i = 0;
+    while (s[0] != '\0')
+    {
+            while(s[0] != '\0' && s[0] == c)
+                s++;
+            if(s[0] != '\0')
+            {
+                len = 0;
+                while (s[len] != '\0' && s[len] != c)
+                    len++;
+                tab[i] = ft_substr(s, 0, len);
+                i++;
+
+                s = s + len;
+            }
+    }
+    tab[i] = NULL;
+    return (tab);
+
+}
+
+int main()
+{
+    char s[] = " this   is  new string  ";
+    char c = ' ';
+    char **result;
+    int i;
+
+    result = ft_split(s, c);
+
+    if (result == NULL)
+        return (1);
+
+    i = 0; 
+    while (result[i] != NULL)
+    { 
+        printf("Word on index %d: %s \n", i, result[i]);
+        i++;
+    }
+
+    i = 0;
+    while (result[i] != NULL)
+    {
+        free(result[i]);
+        i++;
+    }
+    free(result);
+    
+    return (0);
+
+}
+
+
+
+
+
+
+
+
+
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -3,31 +3,49 @@
 #include <stdlib.h>
 #include "libft.h"
 
+
 char *ft_strtrim(char const *s1, char const *set)
 {
-    int start;
-    int end;
-
-    if (s1 == NULL || set == NULL)
-		return (NULL);
-
-    start = 0;
-    while (s1[start] != '\0' && ft_strchr(set, s1[start]) != NULL)
-		start++;
-
-end = ft_strlen(s1) - 1;
-	while (end > start && ft_strchr(set, s1[end]) != NULL)
-		end--;
 
 
-    return (ft_substr(s1, start, (end - start) + 1));
+  int start;
+  int end;
+  
+  if (s1 == NULL || set == NULL)
+    return (NULL);
+
+  start = 0;
+
+  while (s1[start] != '\0' && ft_strchr(set, s1[start]))
+    start++;
+  
+  end = ft_strlen(s1) - 1; 
+  while (end > start && ft_strchr(set, s1[end]))
+         end--;
+
+  if (start > end)
+      return (ft_substr(s1, 0, 0));
+
+  return(ft_substr(s1, start, ((end - start) + 1)));
+  
+
 }
-int main ()
+
+int main()
 {
-    char s1[] = "xxxMARKOxxx";
-    char x[] = "x";
+  char str[] = "xxxxMarkoxxxx";
+  char x[] = "x";
+  char *res;
 
-    printf("this is the new trimmed string %s \n", ft_strtrim(s1, x));
-    return (0);
+  res = ft_strtrim(str, x);
+  printf("this is shorter string, %s \n", res);
+  free (res);
+  return (0);
 }
+
+
+
+
+
+
 
