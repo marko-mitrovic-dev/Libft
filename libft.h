7c0e288cb1b153c1039af6a933b8902d221fc706
Mitre341 <emarkomitrovic@gmail.com>
2026-07-30 18:46:54 +0200

    finished ft_lstlast.c

--- libft.h
+++ libft.h
@@ -48,5 +48,6 @@ void ft_putendl_fd(char *s, int fd);
 t_list *ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int ft_lstsize(t_list *lst);
+t_list *ft_lstlast(t_list *lst);
 
 # endif
